6092b27628c5e5932dbaebce340ea9543a566670
riedsteeve <145045537+riedsteeve@users.noreply.github.com>
2026-09-11 00:35:20 +0200

    Création du composant pour l'affichage d'une Pop Up pour guider les utilisateurs

---
+++ frontend-Tutapp/Tutapp/src/app/petit-tuto-step/petit-tuto-step.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-petit-tuto-step',
+  imports: [],
+  templateUrl: './petit-tuto-step.html',
+  styleUrl: './petit-tuto-step.css',
+})
+export class PetitTutoStep {}
